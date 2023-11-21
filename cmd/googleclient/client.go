e2b53f3f59622cdf536e66c84892afae117629ee
Enrico Ruggieri <enr.ruggieri.mail@gmail.com>
2023-11-21 08:58:34 +0100

    get credentials from env variables

--- cmd/googleclient/client.go
+++ cmd/googleclient/client.go
@@ -2,12 +2,14 @@ package googleclient
 
 import (
 	"context"
+	"errors"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/calendar/v3"
 	"google.golang.org/api/option"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -16,12 +18,22 @@ type config struct {
 	timeZone   string
 }
 
+var jsonCredentials string
+
 type GClient struct {
 	GEvent    GEventService
 	GCalendar GCalendarService
 }
 
+// NewClient create a client from a context and the credentials file path. The credentials file path
+// can be omitted if the GOOGLE_CREDENTIALS env variable is set instead
 func NewClient(ctx context.Context, keyPath string) (*GClient, error) {
+	jsonCredentials = os.Getenv("GOOGLE_CREDENTIALS")
+	if keyPath == "" {
+		if jsonCredentials == "" {
+			return nil, errors.New("one of key path or GOOGLE_CREDENTIALS env must be define")
+		}
+	}
 
 	client, err := getClient(keyPath)
 	if err != nil {
@@ -53,12 +65,22 @@ func NewClient(ctx context.Context, keyPath string) (*GClient, error) {
 
 // getClient with service account generated key
 func getClient(keyPath string) (*http.Client, error) {
+	var err error
+	var credByte []byte
 
-	f, err := ioutil.ReadFile(keyPath)
+	if jsonCredentials != "" {
+		credByte = []byte(jsonCredentials)
+	} else {
+		credByte, err = ioutil.ReadFile(keyPath)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	cred, err := google.CredentialsFromJSON(context.Background(), credByte, "https://www.googleapis.com/auth/calendar")
 	if err != nil {
 		return nil, err
 	}
-	cred, err := google.CredentialsFromJSON(context.Background(), f, "https://www.googleapis.com/auth/calendar")
 	c := oauth2.NewClient(context.Background(), cred.TokenSource)
 	return c, nil
 }
